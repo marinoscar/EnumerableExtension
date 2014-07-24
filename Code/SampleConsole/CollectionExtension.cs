2b33646dcdfbec346d01f58f1ece6f4f539c57fe
Oscar Marin <oscar.marin@entegreat.com>
2014-07-24 16:46:57 -0600

    Initial commit

---
+++ Code/SampleConsole/CollectionExtension.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleConsole
+{
+    public static class CollectionExtension
+    {
+        public static void Iterate<T>(this IEnumerable<T> collection, Action<T> method) where T : class
+        {
+            var list = new List<T>();
+            list.AddRange(collection);
+            for (var i = 0; i < list.Count; i++)
+            {
+                var item = i < list.Count ? list[i] : default(T);
+                if(item != default(T))
+                    method(item);
+            }
+        }
+    }
+
+    public class ListItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Id: {0} Name: {1}", Id, Name);
+        }
+    }
+
+    public class TestListExtension
+    {
+
+        public List<ListItem> InnerList { get; private set; }
+    
+
+        public TestListExtension()
+        {
+            LoadList();
+        }
+
+        private void LoadList()
+        {
+            InnerList = new List<ListItem>()
+                {
+                    new ListItem(){Id = 1, Name = "Oscar"},
+                    new ListItem(){Id = 2, Name = "Mike"},
+                    new ListItem(){Id = 3, Name = "Chris"},
+                    new ListItem(){Id = 4, Name = "Hipolito"},
+                    new ListItem(){Id = 5, Name = "Facundo"}
+                };
+        }
+
+        /// <summary>
+        /// Test the iteration of a collection adding 2 items
+        /// </summary>
+        public void TestIterationAddingItems()
+        {
+            LoadList();
+            InnerList.Iterate(item =>
+                {
+                    if(item.Id == 3)
+                        InnerList.Add(new ListItem()
+                            {
+                                Id = InnerList.Count + 1,
+                                Name = string.Format("New Item Id {0}", InnerList.Count + 1)
+                            });
+                });
+        }
+
+        public void TestIterationRemoving1Item()
+        {
+            LoadList();
+            InnerList.Iterate(RemoveItem);
+        }
+
+        private void RemoveItem(ListItem item)
+        {
+            if (item.Id == 2)
+                InnerList.Remove(item);
+        }
+    }
+}
---
+++ Code/SampleConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PLEASE DEBUG TO UNDERSTAND HOW IT WORKS
+            var listTest = new TestListExtension();
+
+            //This is a simple way to iterate fwd in a list
+            Console.WriteLine("Simple list itearation");
+            ShowList(listTest.InnerList);
+
+            Console.WriteLine("Iterating the list after adding new items");
+            listTest.TestIterationAddingItems();
+            ShowList(listTest.InnerList);
+
+            //Here is a sample on how to iterate in the list when removing 1 item
+            Console.WriteLine("Iterating the list while removing 1 item");
+            listTest.TestIterationRemoving1Item();
+            ShowList(listTest.InnerList);
+
+            Console.WriteLine();
+            Console.Write("Press any key to exit... ");
+            Console.ReadKey();
+
+        }
+
+        static void ShowList(IEnumerable<ListItem> list)
+        {
+            Console.WriteLine();
+            list.Iterate(Console.WriteLine);
+            Console.WriteLine();
+        }
+    }
+}
